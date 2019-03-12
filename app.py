fef067d64a9859292d41d20e0b59fac9bc2f8143
Ralph Marschall <marschallralph@gmail.com>
2019-03-12 20:17:29 +0100

    Add env variables for logs and model

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask
 from waitress import serve
+import os
 from logging.config import dictConfig
 
 from sklearn.externals import joblib
+
+APPLICATION_LOG     = 'logs/app.log'
+APPLICATION_MODEL   = 'model/model.joblib'
+
+ENV_LOG = os.environ.get('APP_LOG')
+if ENV_LOG:
+    APPLICATION_LOG = ENV_LOG
+
+ENV_MODEL = os.environ.get('APP_MODEL')
+if ENV_MODEL:
+    APPLICATION_MODEL = ENV_MODEL
 
 dictConfig({
     'version': 1,
@@ -14,7 +26,7 @@ dictConfig({
     'handlers': {
         'file': {
             'class': 'logging.handlers.RotatingFileHandler',
-            'filename': 'logs/app.log',
+            'filename': APPLICATION_LOG,
             'formatter': 'default'
         }
     },
@@ -25,7 +37,7 @@ dictConfig({
 })
 
 # Models
-insurance_grade_model = joblib.load('model/model.joblib').set_params(n_jobs=1)
+insurance_grade_model = joblib.load(APPLICATION_MODEL).set_params(n_jobs=1)
 
 def create_app():
 
@@ -40,6 +52,8 @@ def create_app():
     app.register_blueprint(insurance.bp)
 
     app.logger.info('App has been created.')
+    app.logger.info(' APPLICATION_LOG   : {}'.format(APPLICATION_LOG))
+    app.logger.info(' APPLICATION_MODEL : {}'.format(APPLICATION_MODEL))
 
     return app
 
